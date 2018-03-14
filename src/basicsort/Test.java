3ac3e0074c9b0248d3090ee3e54e4f1297135440
toujours33 <1012242313@qq.com>
2018-03-14 10:45:08 +0800

    测试函数更新

--- src/basicsort/Test.java
+++ src/basicsort/Test.java
@@ -7,8 +7,10 @@ public class Test {
 	public static void main(String[] args) {
 
 		ArrayOperation<Integer> intUtil = new ArrayOperation<>();
+		ArrayOperation<Character> charUtil = new ArrayOperation<>();
 
-		Integer[] test = new Integer[] { 1, 3, 2, 4, 6, 5, 8, 7, 2, 1, 0 };
+		// Integer[] test = new Integer[] { 1, 3, 2, 4, 6, 5, 8, 7, 2, 1, 0 };
+		Integer[] test = new Integer[] { 6, 5, 3, 1, 8, 7, 2, 4 };
 		intUtil.printArray(intUtil.arrayCopy(test), "未排序数组");
 
 		BubbleSort<Integer> bubbleSort = new BubbleSort<>();
@@ -30,6 +32,30 @@ public class Test {
 		Integer[] copy04 = intUtil.arrayCopy(test);
 		shellSort.sort(copy04);
 		intUtil.printArray(copy04, "希尔排序");
+
+		MergeSort<Integer> mergeSort = new MergeSort<>();
+		Integer[] copy05 = intUtil.arrayCopy(test);
+		mergeSort.sort(copy05);
+		intUtil.printArray(copy05, "归并排序；");
+
+		QuickSort<Integer> quickSort = new QuickSort<>(new QuickSortOneWay<Integer>());
+		Integer[] copy06 = intUtil.arrayCopy(test);
+		quickSort.sort(copy06);
+		intUtil.printArray(copy06, "单向快速排序：");
+
+		QuickSort<Integer> quickSort2 = new QuickSort<>(new QuickSortTwoWay<Integer>());
+		Integer[] copy07 = intUtil.arrayCopy(test);
+		quickSort2.sort(copy07);
+		intUtil.printArray(copy07, "双向快速排序：");
+		
+		Character[] charTest = new Character[] {'v','a','r','b','h','q','p','l','d'};
+		charUtil.printArray(charTest, "未排序字符数组：");
+		
+		QuickSort<Character> quickSort3 = new QuickSort<>(new QuickSortOneWay<Character>());
+		Character[] copy08 = charUtil.arrayCopy(charTest);
+		quickSort3.sort(copy08);
+		charUtil.printArray(copy08, "单向快速排序字符数组：");
+		
 	}
 
 }
